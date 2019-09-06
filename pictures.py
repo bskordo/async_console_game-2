269cfa4b6072eb6648e57425525375b06c844c98
samstudy <samat.study@gmail.com>
2019-09-06 10:50:08 +0600

    Replace tabs to indendents

--- pictures.py
+++ pictures.py
@@ -5,16 +5,17 @@ def read_file(file_path):
 
 
 def get_garbages():
-	files = ['garbages/trash_large.txt', 'garbages/trash_small.txt', 'garbages/trash_xl.txt']
-	garbages = [read_file(file)for file in files]
-	return garbages
+    files = ['garbages/trash_large.txt', 'garbages/trash_small.txt', 'garbages/trash_xl.txt']
+    garbages = [read_file(file)for file in files]
+    return garbages
 
 
 def get_frames():
-	files = ['frames/rocket_frame_1.txt','frames/rocket_frame_2.txt']
-	frames = (read_file(file) for file in files)
-	return frames
+    files = ['frames/rocket_frame_1.txt', 'frames/rocket_frame_2.txt']
+    frames = (read_file(file) for file in files)
+    return frames
+
 
 def get_game_over_text():
-	text = read_file('frames/game_over.txt')
-	return text
+    text = read_file('frames/game_over.txt')
+    return text
